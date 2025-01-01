0fdd95c0122d767c4c31ccd56bc5506078701131
danielwmartin1 <danielwmartin1@gmail.com>
2025-01-01 10:00:58 -0500

    Improve MongoDB connection handling: add error checking for MONGODB_URI and log connection attempts

--- utils/db.js
+++ utils/db.js
@@ -3,7 +3,18 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) return;
 
-  return mongoose.connect(process.env.MONGODB_URI, {});
+  const mongoUri = process.env.MONGODB_URI;
+  if (!mongoUri) {
+    console.error("MONGODB_URI is not defined in environment variables");
+    throw new Error("MONGODB_URI is not defined in environment variables");
+  }
+
+  console.log("Connecting to MongoDB with URI:", mongoUri);
+
+  return mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
 
 export default connectDB;
