00a1ecb310f4742a454e607b9dfb3e34e7a221fa
danielwmartin1 <danielwmartin1@gmail.com>
2025-01-01 10:29:01 -0500

    Remove task management API implementation and update MongoDB URI variable name

--- utils/db.js
+++ utils/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
 
   }
 
-  const mongoURI = process.env.MONGO_URI;
+  const mongoURI = process.env.MONGODB_URI;
 
   if (!mongoURI) {
     throw new Error('MONGO_URI is not defined in environment variables');
