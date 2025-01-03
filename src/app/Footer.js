1733142ed122077dddb603c26b38b6ec155db527
danielwmartin1 <danielwmartin1@gmail.com>
2025-01-03 04:50:36 -0500

    Enhance Footer and Header components with bold yellow text for improved visibility

--- src/app/Footer.js
+++ src/app/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-blue-500 text-cyan-100 p-4 mt-4 border-t-4 border-ridge border-cyan-300">
-      <p className="text-center">Daniel Martin &copy; {new Date().getFullYear()}</p>
+      <p className="text-center font-bold text-yellow-300">Daniel Martin &copy; {new Date().getFullYear()}</p>
     </footer>
   );
 }
